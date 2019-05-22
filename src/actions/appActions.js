c520611c9c4a85c11ce9b1923450cc8e34d007fa
AJ Arriola <angelojames.arriola@gmail.com>
2019-05-22 16:43:32 -0700

    created file for toggleButton action

---
+++ src/actions/appActions.js
@@ -0,0 +1,14 @@
+import {
+  TOGGLE_BUTTON
+} from './types';
+
+//import future types into object above from types file
+
+
+//below is where the logic of your action would happen before passing to the reducer
+
+export const toggleButton = () => {
+  return {
+    type: TOGGLE_BUTTON
+  }
+};
