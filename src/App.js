773307d860bc5d210e05237e6ad8a8bf4501bb3f
AJ Arriola <angelojames.arriola@gmail.com>
2019-05-22 17:02:41 -0700

    added button to apply action too and used ternary to toggle between diffrent h1 tags

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { toggleButtonAction } from './actions/appActions';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    const { toggleButton, toggleButtonAction } = this.props;
 
-export default App;
+    return (
+      <div className="App">
+        { toggleButton ? <h1>Hello World</h1> : <h1>Goodbye World</h1> }
+        <button onClick={toggleButtonAction}>
+          CLICKITY CLICK
+        </button>
+      </div>
+    )
+  }
+};
+
+const mapStateToProps = state => ({
+  ...state.app
+});
+
+export default connect(
+  mapStateToProps,
+  { toggleButtonAction }
+)(App);
